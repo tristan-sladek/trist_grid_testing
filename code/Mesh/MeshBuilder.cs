18d6e528bf7c3c4107f6b06ba6f6935602846724
Tristan Sladek <TristanSladek@gmail.com>
2022-02-20 15:27:43 -0600

    Cleaned up Mesh Builder

--- code/Mesh/MeshBuilder.cs
+++ code/Mesh/MeshBuilder.cs
@@ -5,6 +5,50 @@ public class MeshBuilder
 {
 	float MAX = float.MinValue;
 	float MIN = float.MaxValue;
+	private GridWorld CurrentGridWorld;
+	private int ChunkX;
+	private int ChunkY;
+	public Model Build( GridWorld currentGridWorld, int chunkX, int chunkY )
+	{
+		CurrentGridWorld = currentGridWorld;
+		ChunkX = chunkX;
+		ChunkY = chunkY;
+
+		var m1 = Material.Load( "dev/helper/testgrid.vmat" );
+		var m2 = Material.Load( "materials/dev/dev_measuregeneric01.vmat" );
+		var m3 = Material.Load( "materials/dev/dev_measuregeneric01b.vmat" );
+		var m4 = Material.Load( "materials/dev/debug_wireframe.vmat" );
+		var m5 = Material.Load( "materials/vertex_lookup.vmat" );
+
+		return new ModelBuilder()
+			.AddMesh( CreateMesh( FloorPass(), m5 ) )
+			.AddMesh( CreateMesh( FloorWallPass(), m5 ) )
+			.AddMesh( CreateMesh( WallPass(), m5 ) )
+			.Create();
+	}
+	private Mesh CreateMesh( List<TristVertex> verts, Material mat )
+	{
+		Mesh mesh = null;
+		if ( verts.Count > 0 )
+		{
+			mesh = new Mesh( mat ) { Bounds = new BBox( MIN, MAX ) };
+			mesh.CreateVertexBuffer<TristVertex>( verts.Count, TristVertex.Layout, verts );
+		}
+		return mesh;
+	}
+	private void CheckMinMax( params float[] v )
+	{
+		foreach ( var val in v )
+		{
+			if ( val < MIN ) MIN = val;
+			if ( val > MAX ) MAX = val;
+		}
+	}
+	// Main quad function, handles generating Normals and Tangent from vectors;
+	// Params: v1 - v4 are coordinates in world space, in order it's x,y | x + 1, y | x, y + 1 | x + 1, y + 1
+	// TI : Texture Index, this value controls the ID of the texture being pulled from the Texture Atlas material
+	// W, H, OX, OY : These are Width, Height, Offset X, Offset Y; they control the subset drawing of the UV map
+	//					Note, setting W or H over 1 will loop it on materials, useful for repeated textures.
 	private TristVertex[] GetQuad( Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, uint ti = 1, float w = 1, float h = 1, float ox = 0, float oy = 0 )
 	{
 		Vector3 N = (v1 - v2).Cross( v1 - v3 ) * -1;
@@ -17,21 +61,14 @@ public class MeshBuilder
 		
 		return new TristVertex[]{ a, c, b, d, b, c };
 	}
-	private void CheckMinMax(params float[] v)
-	{
-		foreach(var val in v)
-		{
-			if ( val < MIN ) MIN = val;
-			if ( val > MAX ) MAX = val;
-		}		
-	}
+	// This loops over all tiles to generate the floor quads based upon their height.
 	private List<TristVertex> FloorPass()
 	{
 		List<TristVertex> fVerts = new();
 		for ( int x = 0; x < GridWorld.CHUNK_SIZE; x++ )
 			for ( int y = 0; y < GridWorld.CHUNK_SIZE; y++ )
 			{
-				var t = getTile(x,y);
+				var t = GetTile(x,y);
 				var verts = Floor( x, y, t.Height, GridWorld.GRID_SCALE );
 				if ( verts != null && verts.Length > 0 )
 					foreach ( var v in verts )
@@ -40,93 +77,101 @@ public class MeshBuilder
 				
 		return fVerts;
 	}
-	private List<TristVertex> HorizontalPass()
+	// This generates all walls inbetween differring floor heights
+	private List<TristVertex> FloorWallPass()
 	{
-		List<TristVertex> hVerts = new();
-		for ( int y = 0; y < GridWorld.CHUNK_SIZE; y++)
-			for (int x = 0;x < GridWorld.CHUNK_SIZE; x++ )
+		var CS = GridWorld.CHUNK_SIZE;
+		List<TristVertex> fVerts = new();
+		for(int y = 0; y < CS; y++ )
+			for(int x = 0; x < CS; x++ )
 			{
-				var TA = getTile(x,y);
-				var TB = getTile(x + 1,y);
-				if ( TA.Height == TB.Height ) continue;
-				float x1 = GridWorld.GRID_SCALE;
-				float y1 = GridWorld.GRID_SCALE;
-				float y2 = GridWorld.GRID_SCALE;
-				float z1 = GridWorld.GRID_SCALE;
-				float z2 = GridWorld.GRID_SCALE;
-				if ( TA.Height > TB.Height )
+				var T = GetTile( x, y );		//Current Tile
+				var TE = GetTile( x + 1, y );	// East Tile
+				var TN = GetTile( x, y + 1 );	// North Tile
+
+				// Can't use implicit else because quad drawn on TN.Height == T.Height
+				if(TN.Height > T.Height)
 				{
-					x1 *= x + 1;
-					y1 *= y;
-					y2 *= y + 1;
-					z1 *= TB.Height;
-					z2 *= TA.Height;
-				}
-				else
-				{
-					x1 *= x + 1;
-					y1 *= y + 1;
-					y2 *= y;
-					z1 *= TA.Height;
-					z2 *= TB.Height;
+					// TODO: replace texture index with tile's attribute
+					var verts = FloorWall( x, y, T.Height, (TN.Height - T.Height), 0, 1 );
+					foreach ( var v in verts )
+						fVerts.Add( v );
 				}				
-				var verts = GetQuad(
-					new Vector3( x1, y1, z1 ),
-					new Vector3( x1, y1, z2 ),
-					new Vector3( x1, y2, z1 ),
-					new Vector3( x1, y2, z2 ), 1,
-					1,
-					Math.Abs( TA.Height - TB.Height )
-				);
-				foreach(var v in verts)
-				hVerts.Add( v );
-			}		
-		return hVerts;
+				else if (TN.Height < T.Height)
+				{
+					var verts = FloorWall( x, y + 1, TN.Height, (T.Height - TN.Height), 1, 1 );
+					foreach ( var v in verts )
+						fVerts.Add( v );
+				}
+				
+				if ( TE.Height > T.Height )
+				{
+					var verts = FloorWall( x, y, T.Height, (TE.Height - T.Height), 1.5f, 1 );
+					foreach ( var v in verts )
+						fVerts.Add( v );
+				}
+				else if ( TE.Height < T.Height )
+				{
+					var verts = FloorWall( x + 1, y, TE.Height, (T.Height - TE.Height), 0.5f, 1 );
+					foreach ( var v in verts )
+						fVerts.Add( v );
+				}
+				
+
+			}
+		return fVerts;
 	}
-	private List<TristVertex> VerticalPass()
+	// This generates walls based upon the tile's Wall Attribute. 0b1111 in order of walls is 0bNSEW
+	private List<TristVertex> WallPass()
 	{
-		List<TristVertex> vVerts = new();
+		List<TristVertex> wVerts = new();
+
 		for ( int y = 0; y < GridWorld.CHUNK_SIZE; y++ )
 			for ( int x = 0; x < GridWorld.CHUNK_SIZE; x++ )
 			{
-				var TA = getTile( x, y );
-				var TB = getTile( x, y + 1 );
-				if ( TA.Height == TB.Height ) continue;				
-				float x1 = GridWorld.GRID_SCALE;
-				float x2 = GridWorld.GRID_SCALE;
-				float y1 = GridWorld.GRID_SCALE;
-				float z1 = GridWorld.GRID_SCALE;
-				float z2 = GridWorld.GRID_SCALE;
-				if ( TA.Height > TB.Height )
-				{					
-					x1 *= x + 1;
-					x2 *= x;
-					y1 *= y + 1;
-					z1 *= TB.Height;
-					z2 *= TA.Height;
+				var T = GetTile( x, y );
+				bool N = T.NorthWall;
+				bool S = T.SouthWall;
+				bool E = T.EastWall;
+				bool W = T.WestWall;
+				bool NW = GetTile( x, y + 1 ).SouthWall; // north tile has south wall, ect..
+				bool SW = GetTile( x, y - 1 ).NorthWall;
+				bool EW = GetTile( x + 1, y ).WestWall;
+				bool WW = GetTile( x - 1, y ).EastWall;
+
+				List<TristVertex> verts;
+
+				if ( N )
+				{
+					verts = WallSeg( x, y, T.Height, N, E, W, NW, EW, WW, 0 );
+					foreach ( var v in verts )
+						wVerts.Add( v );
+				}				
+				if ( S )
+				{
+					verts = WallSeg( x, y, T.Height, S, W, E, SW, WW, EW, 1 );
+					foreach ( var v in verts )
+						wVerts.Add( v );
 				}
-				else
-				{					
-					x1 *= x;
-					x2 *= x + 1;
-					y1 *= y + 1;
-					z1 *= TA.Height;
-					z2 *= TB.Height;
+				if ( E )
+				{
+					verts = WallSeg( x, y, T.Height, E, S, N, EW, SW, NW, 1.5f );
+					foreach ( var v in verts )
+						wVerts.Add( v );
 				}
-				var verts = GetQuad(
-					new Vector3( x1, y1, z1 ),
-					new Vector3( x1, y1, z2 ),
-					new Vector3( x2, y1, z1 ),
-					new Vector3( x2, y1, z2 ), 1,
-					1,
-					Math.Abs( TA.Height - TB.Height )
-				);
-				foreach ( var v in verts )
-					vVerts.Add( v );
+				if ( W )
+				{
+					verts = WallSeg( x, y, T.Height, W, N, S, WW, NW, SW, 0.5f );
+					foreach ( var v in verts )
+						wVerts.Add( v );
+				}
+				
 			}
-		return vVerts;
+
+		return wVerts;
 	}
-	private Vector3 Rotate(Vector3 vec, float ang)
+	// Rotates a vector's X Y coordinates by 1 radian
+	private Vector3 Rotate( Vector3 vec, float ang )
 	{
 		return new Vector3(
 			vec.x * MathF.Cos( ang * MathF.PI ) - vec.y * MathF.Sin( ang * MathF.PI ),
@@ -134,14 +179,72 @@ public class MeshBuilder
 			vec.z
 			);
 	}
-	private List<TristVertex> WallSeg(int x, int y, float h, bool N, bool E, bool W, bool NW, bool EW, bool WW, float R)
+	// Retrieves a tile from the world using GRID SPACE coordinates
+	private Tile GetTile( int x, int y )
+	{
+		int cx = ChunkX;
+		int cy = ChunkY;
+		if ( x < 0 ) { cx--; x += GridWorld.CHUNK_SIZE; }
+		if ( y < 0 ) { cy--; y += GridWorld.CHUNK_SIZE; }
+		if ( x >= GridWorld.CHUNK_SIZE ) { cx++; x -= GridWorld.CHUNK_SIZE; }
+		if ( y >= GridWorld.CHUNK_SIZE ) { cy++; y -= GridWorld.CHUNK_SIZE; }
+		return CurrentGridWorld.GetTile( new Vector4( x, y, cx, cy ) );
+	}
+	private TristVertex[] Floor( float ix, float iy, float iz, float s )
+	{
+		var x = ix * s;
+		var y = iy * s;
+		var z = iz * s;
+		CheckMinMax( x, y, z );
+
+		var v = GetQuad(
+			new Vector3( x, y, z ),
+			new Vector3( x, y + s, z ),
+			new Vector3( x + s, y, z ),
+			new Vector3( x + s, y + s, z )
+			, 0
+		);
+		
+		return v;
+	}
+	private TristVertex[] FloorWall ( float x, float y, float z, float h, float r, uint tI = 1)
+	{
+		// Provides functionality for building a quad on the south face of a tile,
+		// allows for rotation to support E,W, and S tiles.
+
+		var GS = GridWorld.GRID_SCALE;
+		var HGS = GS / 2;
+
+		var x1 = -HGS;
+		var x2 = HGS;
+		var y1 = HGS;
+		var z2 = h * GS;
+
+		// offset to move tile by when it is calculated
+		var off = new Vector3( x * GS + HGS, y * GS + HGS, z * GS );
+
+		// Vectors calculated from the perspective of 0,0 being the tile to rotate from
+		var a = Rotate( new Vector3( x1, y1, 0 ), r);
+		var b = Rotate( new Vector3( x1, y1, z2  ), r);
+		var c = Rotate( new Vector3( x2, y1, 0 ), r);
+		var d = Rotate( new Vector3( x2, y1, z2 ), r);
+		
+		// Local tile moved to sit at proper offset;
+		a += off;
+		b += off;
+		c += off;
+		d += off;
+
+		return GetQuad( a, b, c, d, tI, 1, h );
+	}
+	private List<TristVertex> WallSeg( int x, int y, float h, bool N, bool E, bool W, bool NW, bool EW, bool WW, float R )
 	{
 		float GS = GridWorld.GRID_SCALE;
 		float HGS = GS / 2;
 		List<TristVertex> wVerts = new();
 
 		float z1 = h * GS;
-		float z2 = (h + 1) * GS;		
+		float z2 = (h + 1) * GS;
 		float SL = 0.375f; //Scale Large (how many units off the wall?)
 		float SS = 0.5f - SL; //Scale Small (inverse of this)
 		TristVertex[] verts;
@@ -169,7 +272,7 @@ public class MeshBuilder
 				verts = GetQuad( a, b, c, d, 2, (x2 - x1) / GS, 1, (x1 + HGS) / GS );//, (x2 - x1) / GS, 1, (x1 / GS - x) );
 				foreach ( var v in verts )
 					wVerts.Add( v );
-			}			
+			}
 
 			// TOP
 			{
@@ -188,14 +291,14 @@ public class MeshBuilder
 				d += off;
 
 				verts = GetQuad( a, b, c, d, 2, (x2 - x1) / GS, SS, (x1 + HGS) / GS );
-				
-				
+
+
 				foreach ( var v in verts )
 					wVerts.Add( v );
 			}
 
 			// BACK
-			if( !NW )
+			if ( !NW )
 			{
 				float x1 = HGS;
 				float x2 = -HGS;
@@ -214,7 +317,7 @@ public class MeshBuilder
 					wVerts.Add( v );
 			}
 
-			if( !EW & !E )
+			if ( !EW & !E )
 			{
 				float x1 = HGS;
 				float y1 = HGS - SS * GS;
@@ -255,116 +358,5 @@ public class MeshBuilder
 		}
 
 		return wVerts;
-	}
-	private List<TristVertex> WallPass()
-	{
-		List<TristVertex> wVerts = new();
-
-		for ( int y = 0; y < GridWorld.CHUNK_SIZE; y++ )
-			for ( int x = 0; x < GridWorld.CHUNK_SIZE; x++ )
-			{
-				var T = getTile( x, y );
-				bool N = T.NorthWall;
-				bool S = T.SouthWall;
-				bool E = T.EastWall;
-				bool W = T.WestWall;
-				bool NW = getTile( x, y + 1 ).SouthWall; // north tile has south wall, ect..
-				bool SW = getTile( x, y - 1 ).NorthWall;
-				bool EW = getTile( x + 1, y ).WestWall;
-				bool WW = getTile( x - 1, y ).EastWall;
-
-				List<TristVertex> verts;
-
-				if ( N )
-				{
-					verts = WallSeg( x, y, T.Height, N, E, W, NW, EW, WW, 0 );
-					foreach ( var v in verts )
-						wVerts.Add( v );
-				}				
-				if ( S )
-				{
-					verts = WallSeg( x, y, T.Height, S, W, E, SW, WW, EW, 1 );
-					foreach ( var v in verts )
-						wVerts.Add( v );
-				}
-				if ( E )
-				{
-					verts = WallSeg( x, y, T.Height, E, S, N, EW, SW, NW, 1.5f );
-					foreach ( var v in verts )
-						wVerts.Add( v );
-				}
-				if ( W )
-				{
-					verts = WallSeg( x, y, T.Height, W, N, S, WW, NW, SW, 0.5f );
-					foreach ( var v in verts )
-						wVerts.Add( v );
-				}
-				
-			}
-
-		return wVerts;
-	}
-	private Tile getTile( int x, int y )
-	{
-		int cx = ChunkX;
-		int cy = ChunkY;
-		if ( x < 0 ) { cx--; x += GridWorld.CHUNK_SIZE; }
-		if ( y < 0 ) { cy--; y += GridWorld.CHUNK_SIZE; }
-		if ( x >= GridWorld.CHUNK_SIZE ) { cx++; x -= GridWorld.CHUNK_SIZE; }
-		if ( y >= GridWorld.CHUNK_SIZE ) { cy++; y -= GridWorld.CHUNK_SIZE; }
-		return CurrentGridWorld.GetTile( new Vector4( x, y, cx, cy) );
-	}
-	private GridWorld CurrentGridWorld;
-	private int ChunkX;
-	private int ChunkY;
-	public Model Build( GridWorld currentGridWorld, int chunkX, int chunkY )
-	{
-		CurrentGridWorld = currentGridWorld;
-		ChunkX = chunkX;
-		ChunkY = chunkY;
-
-		var wall_verts = HorizontalPass();
-		var temp = VerticalPass();
-		foreach ( var v in temp )
-			wall_verts.Add( v );
-
-		var m1 = Material.Load( "dev/helper/testgrid.vmat" );
-		var m2 = Material.Load( "materials/dev/dev_measuregeneric01.vmat" );
-		var m3 = Material.Load( "materials/dev/dev_measuregeneric01b.vmat" );
-		var m4 = Material.Load( "materials/dev/debug_wireframe.vmat" );
-		var m5 = Material.Load( "materials/vertex_lookup.vmat" );
-
-		return new ModelBuilder()
-			.AddMesh( CreateMesh( FloorPass(), m5 ) )
-			.AddMesh( CreateMesh( wall_verts, m5 ) )
-			.AddMesh( CreateMesh( WallPass(), m5 ) )
-			.Create();
-	}
-	private Mesh CreateMesh( List<TristVertex> verts, Material mat)
-	{
-		Mesh mesh = null;
-		if ( verts.Count > 0 )
-		{
-			mesh = new Mesh( mat ) { Bounds = new BBox( MIN, MAX ) };
-			mesh.CreateVertexBuffer<TristVertex>( verts.Count, TristVertex.Layout, verts );
-		}
-		return mesh;
-	}
-	public TristVertex[] Floor( float ix, float iy, float iz, float s )
-	{
-		var x = ix * s;
-		var y = iy * s;
-		var z = iz * s;
-		CheckMinMax( x, y, z );
-
-		var v = GetQuad(
-			new Vector3( x, y, z ),
-			new Vector3( x, y + s, z ),
-			new Vector3( x + s, y, z ),
-			new Vector3( x + s, y + s, z )
-			, 0
-		);
-		
-		return v;
 	}
 }
